3802085c72e45d3ffe4e41e13d253a1b81044a83
Abraham Elias <abraham.elias12@gmail.com>
2019-08-01 19:23:31 +1000

    added comments and minor changes to logic.

--- logic.py
+++ logic.py
@@ -1,36 +1,45 @@
 import itertools, html, math
 
-def is_Collision(session, other):
-
-    for day in session["days"]:
-        if day in other["days"]:
-            if(max(session["start"], other["start"]) < min(session["end"], other["end"])):
+"""
+    HELPER FUNCTIONS
+"""
+# If classA and classB are on the same days, checks to make sure their times do not overlap.
+# Returns true if there is a collision.
+def is_Collision(classA, classB):
+    for day in classA["days"]:
+        if day in classB["days"]:
+            if(max(classA["start"], classB["start"]) < min(classA["end"], classB["end"])):
                 return True
-            else:
-                return False
     return False
 
+# Converts times from strings in the format hh:mm to an int with format hhmm
+# If there is also no end time given, end time is set to an hour after start time (+ 100 on the int)
 def convert_times(start, end):
     newStart = int(start.replace(":", ""))
     newEnd = newStart + 100 if end == None else int(end.replace(":", ""))
     return newStart, newEnd
 
+# Converts that int time back to string format hh:mm
 def display_time(time):
     time = str(time)
     return time[:-2] + ":" + time[-2:]
 
-def already_contains(item, entire):
-    for x in entire:
+# Return true if 'item' is in wholeset
+# Used for when 'item' is a class option, and wholeset is an array of options
+def already_contains(item, wholeset):
+    for x in wholeset:
         if x["days"] == item["days"] and x["start"] == item["start"]:
             return True
     return False
 
+
+"""
+    MAIN FUNCTION CALLED BY THE FLASK APP
+"""
 def do_work(classes):
 
-    response = {
-        "results": [],
-        "error": None
-    }
+    # Goes through all the classes, cleans up the options for that class and adds them to an array
+    # Adds that array to all_options, which becomes an array of option arrays
     all_options = []
     for _name, class_ in classes.items():
         current_options = []
@@ -43,14 +52,14 @@ def do_work(classes):
                 current_options.append(o)
         all_options.append(current_options)
 
+    # Generate all combinations of options
     results = list(itertools.product(*all_options))
 
+    # Iterates over all possible results, and only keeps those with the least number of days
     best_days = 8
     least_days = []
-    for week in reversed(results):
-        # Iterates over all possible results, 
-        # and only keeps those with the least number of days, 
-        # removing the rest
+    for week in results:
+        # Array hold all days that have a class on them
         days = []
         for session in week:
             for day in session["days"]:
@@ -62,11 +71,11 @@ def do_work(classes):
         elif len(days) == best_days:
             least_days.append(week)
 
-    # Iterates over all remaining options,
+    # Iterates over all the weeks with the least days,
     # compares each session to every other session in that week,
     # only keeps the week if it does not contain a collision
     no_collisions = []
-    for week in reversed(least_days):
+    for week in least_days:
         safe = True
         for i in range(len(week)):
             session = week[i]
@@ -77,17 +86,19 @@ def do_work(classes):
         if safe:
             no_collisions.append(week)
     
+    # SCORING ALGORITHM
+    # All the remaining weeks that do not have collisions are scored.
     scored = []
     max_score = 0
     best_scorers = []
     for week in no_collisions:
 
-        # Keep track of points for each day
+        # Tracks points for each day in the week [mon, tues, wed, thur, fri]
         daily_points = [0,0,0,0,0]
-
-        # Tracks how many classes per day
+        # Tracks how many classes per day [mon, tues, wed, thur, fri]
         daily_classes = [0,0,0,0,0]
 
+        # Here each class is scored based on its start time and class type
         for session in week:
             # Points for this session
             points = 0
@@ -95,11 +106,12 @@ def do_work(classes):
             # Add the start time, later start times are more favourable
             points += session["start"] ** 2
 
-            # Lectures are more favourable, more skipable
+            # Lectures are favourbale and score higher because they are skipable lol
             if session["type"].lower() in ['lecture', "lec", "l"]:
-                points += 10
+                points += 100
 
-            # Add sessions points to all days the session is on
+            # Add this sessions points to the day(s) it is on 
+            # and also +1 classes for that day
             for day in session["days"]:
                 day = day.lower()
                 if day == "mon":
@@ -123,20 +135,33 @@ def do_work(classes):
         
         # Measure of how spread out the classes are over the days
         # Smaller variance is more favourable
+        """
+            idk if this is even right but its all i could come up with, 
+            basically non-zero is daily_classes but with all the 0's removed, so it may look like [2,4,5]. 
+            These numbers are then all multiplied by eachother. The idea is that weeks with the same number
+            of classes can get different scores based on if the classes are bulked into one day or not. eg 
+            [3, 3, 3, 3, 3] = 243, [5, 1, 4, 2, 3] = 120, therefore the second one is more favourable, 
+            even though they both have the same number of classes.
+        """
         non_zero = [float(v) for v in daily_classes if v != 0]
         variance = 1
         for x in non_zero:
             variance *= x
 
-        # Final score, higher is better
-        score = max(daily_points) - math.sqrt(variance)
+        # Final score, higher is better, therefore you want a high daily_point and low variance
+        """
+            maybe max(daily_points) should change to a sum or maybe a product?
+            i feel like i had that before but it didn't work i don't remember why - needs more investigation
+        """
+        score = max(daily_points) - (variance ** 2)
 
+        # Convert the week (currently a tuple) to a list so it can be mutated
         weekAsList = list(week)
 
-        # Sort by start time
+        # Sort the week by start time so its in a good order
         weekAsList.sort(key = lambda x: x["start"])
 
-        # Add the score
+        # Add the score to the end of the week
         weekAsList.append(score)
         
         # Keep track of those that have the highest score
@@ -146,25 +171,34 @@ def do_work(classes):
         elif score == max_score:
             best_scorers.append(weekAsList)
 
-        # Add all to the 'scored' list
+        # Add get added to the scored list
         scored.append(weekAsList)
     
-    #format response
+    # Begin to formulate the response that gets passed back to the site via AJAX.
+    response = {
+        "results": []
+    }
     for week in best_scorers:
-        days = {
+        week_final = {
             "Mon" : [],
             "Tue": [],
             "Wed": [],
             "Thu": [],
             "Fri": []
         }
-        
+        # Every class is formated then added to the relevant days
         for session in week:
             if type(session) == float:
                 continue
+                
             s = "{}: {}-{}".format(session["name"], display_time(session["start"]), display_time(session["end"]))
+            # Safety first ;)
+            s = html.escape(s)
+            
             for day in session["days"]:
-                days[day].append(s)
-        response["results"].append(days)
+                week_final[day].append(s)
+        # The week_final is added to the response
+        response["results"].append(week_final)
 
+    # All done yay
     return response
