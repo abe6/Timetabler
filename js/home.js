3be0e039c5e81adccf087efea794119253eadadc
Abraham Elias <abraham.elias12@gmail.com>
2019-08-18 13:35:25 +1000

    Started new styling, still sucks

--- js/home.js
+++ js/home.js
@@ -36,7 +36,8 @@ $( "#submitForm" ).submit(function( event ) {
         // Process response here
 
         // Empty existing results and add the new ones
-        $( ".results" ).empty()
+        $( ".nav-tabs" ).empty()
+        $( ".tab-content" ).empty()
         $( ".errors" ).empty()
 
         // Display code errors
@@ -49,54 +50,25 @@ $( "#submitForm" ).submit(function( event ) {
 
         // Handle an empty response
         if(response.results.length == 0){
-          $( ".results" ).append( 
-            `<div class="media border rounded-lg p-2 m-3 w-100">
-                <img src="img/exclamation.png" alt="oops" class="mr-3 mt-3 rounded-circle align-self-top" style="width:60px;">
-                  <div class="media-body">
-                      <h4>No Results! <small><i class="text-secondary">Sorry.</i></small></h4>
-                      <h6>No timetables could be generated.</h6>
-                  </div>
-              </div>`
-          );
+          console.log("ERROR")
         } else {
-            response.results.forEach(result => {
-                var monday = "";
-                for (let i = 0; i < result.Mon.length; i++) {
-                  monday = monday + `<p>${result.Mon[i]}</p>`;
-                }
-                var tuesday = "";
-                for (let i = 0; i < result.Tue.length; i++) {
-                  tuesday = tuesday + `<p>${result.Tue[i]}</p>`;
-                }
-                var wednesday = "";
-                for (let i = 0; i < result.Wed.length; i++) {
-                  wednesday = wednesday + `<p>${result.Wed[i]}</p>`;
-                }
-                var thursday = "";
-                for (let i = 0; i < result.Thu.length; i++) {
-                  thursday = thursday + `<p>${result.Thu[i]}</p>`;
-                }
-                var friday = "";
-                for (let i = 0; i < result.Fri.length; i++) {
-                  friday = friday + `<p>${result.Fri[i]}</p>`;
-                }
-                $( ".results" ).append( 
-                  `<div class="media border rounded-lg p-2 m-3 answer">
-                      <div class="media-body">
-                          <h5>MONDAY</h5>
-                          ${monday}
-                          <h5>TUESDAY</h5>
-                          ${tuesday}
-                          <h5>WEDNESDAY</h5>
-                          ${wednesday}
-                          <h5>THURSDAY</h5>
-                          ${thursday}
-                          <h5>FRIDAY</h5>
-                          ${friday}
-                      </div>
-                  </div>`
-                  );
-              });
+          // Add results
+          console.log(response)
+          response.results.forEach((result, index) => {
+                
+            // Add tab
+            $( ".nav-tabs" ).append(`<li class="nav-item">
+            <a class="nav-link" role="tab" data-toggle="tab" href="#${index}">Result #${index + 1}</a>
+            </li>`)
+
+            // Add content
+            $( ".tab-content" ).append(`<div role="tabpanel" class="tab-pane fade" id="${index}"><br><br>
+            <p>${result.Mon}    #${index+1}</p>
+            </div>`)
+
+          });
+          $( ".nav-tabs" ).find( "a[href='#0']" ).addClass("active")
+          $( ".tab-content" ).find( "div[id='0']" ).removeClass("fade").addClass("active")
         }
     });
   
